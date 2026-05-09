2149ca5050f1a547f3292ab08028f11914843dcc
Latance <platance@alelo.com.br>
2026-05-09 18:28:21 -0300

    feat: classe main do Spring Boot

---
+++ event-catalog/src/main/java/com/eventmaster/catalog/EventCatalogApplication.java
@@ -0,0 +1,12 @@
+package com.eventmaster.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventCatalogApplication.class, args);
+    }
+}
